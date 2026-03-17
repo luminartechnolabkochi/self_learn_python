aa5de839a7b23ac937f41954ec9ff762d039548f
sajay <sajaykannan10@gmail.com>
2026-03-17 14:32:08 +0530

    changed in addion swapping

--- variables/addition.py
+++ variables/addition.py
@@ -1,6 +1,6 @@
 
-num1 = 100
-num2=20
+num1 = int(input("enter num1"))
+num2=int(input("enter num1=2"))
 result = num1+num2
 
 print("addition result",result)
--- variables/swapping.py
+++ variables/swapping.py
@@ -4,10 +4,12 @@ n2 = int(input("enter num1"))
 
 print(f"values b4 swapping n1={n1} n2={n2}")
 
-temp = n1
+# temp = n1
 
-n1=n2
+# n1=n2
 
-n2 = temp
+# n2 = temp
+
+n1,n2=n2,n1
 
 print(f"values after swapping n1={n1} n2={n2}")
