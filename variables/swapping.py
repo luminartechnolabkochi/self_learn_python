0bb5d4f9fbe2582f74f4c5219e04d9ce4355b07c
sajay <sajaykannan10@gmail.com>
2026-03-17 14:28:58 +0530

    swapping program done

---
+++ variables/swapping.py
@@ -0,0 +1,13 @@
+n1 = int(input("enter num1"))
+
+n2 = int(input("enter num1"))
+
+print(f"values b4 swapping n1={n1} n2={n2}")
+
+temp = n1
+
+n1=n2
+
+n2 = temp
+
+print(f"values after swapping n1={n1} n2={n2}")
